25f220c3c37701b767b3ad7c5484ecbec5f90e60
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-15 20:40:39 +0530

    Update familylist.py

--- alluka/modules/familylist.py
+++ alluka/modules/familylist.py
@@ -11,29 +11,17 @@ def ud(bot: Bot, update: Update):
   message = update.effective_message
   text = message.text[len('/familylist '):]
   
-  sunnyimg = "https://telegra.ph/file/4aee5cfe2ba8a3fa503d0.jpg"
-  sunny = """[Sunny ZoldyckFamily」](https://telegram.dog/medevilofmelodies) as Hisoka Morow.\n To get more about him do `!info @medevilofmelodies`"""
+  Devimg = "https://telegra.ph/file/4aee5cfe2ba8a3fa503d0.jpg"
+  Dev = """[Sed Lyf](https://telegram.dog/Ayanokoji_op) as Hisoka Morow.\n To get more about him do `!info @Ayanokoji_op`"""
 
-  neelimg = "https://telegra.ph/file/520c4b38b71f82e312f5b.png"
-  neel = """[Neel ZoldyckFamily」](https://telegram.dog/spookyenvy) as Kite.\n To get more about him do `!info @spookyenvy`"""
-
-  aniimg = "https://telegra.ph/file/d4888fcbdeb3261a2a9cf.png"
-  anii = """[Anii「ZoldyckFamily」](https://telegram.dog/spookyanii) as Chrollo Lucilfer.\n To get more about him do `!info @spookyanii`"""
-  
-  saharshimg = "https://telegra.ph/file/348ae7fcba0116a9a4314.jpg"
-  saharsh = """[Saharsh DHMN! ZoldyckFamily」](https://telegram.dog/MedevilofMarvel) as Meruem.\n To get more about him do `!info @MedevilofMarvel`"""
-
-  kanekiimg = "https://telegra.ph/file/58648d205872f999dcc71.png"
-  kaneki = """[金木健 #UT #LazyAF_Geng #Toxic_Gang](https://telegram.dog/Top_Kekk) as Ging.\n To get more about him do `!info @Top_Kekk`"""
+  Divuimg = "https://telegra.ph/file/520c4b38b71f82e312f5b.png"
+  Divu = """[Mr.Divu」](https://telegram.dog/imDivu) as Kite.\n To get more about him do `!info @imDivu`"""
 
 
  
 
-  message.reply_photo(sunnyimg,sunny, parse_mode=ParseMode.MARKDOWN)
-  message.reply_photo(neelimg, neel, parse_mode=ParseMode.MARKDOWN)  
-  message.reply_photo(aniimg, anii, parse_mode=ParseMode.MARKDOWN)
-  message.reply_photo(saharshimg, saharsh, parse_mode=ParseMode.MARKDOWN)
-  message.reply_photo(kanekiimg, kaneki, parse_mode=ParseMode.MARKDOWN)
+  message.reply_photo(Devimg, Dev, parse_mode=ParseMode.MARKDOWN)
+  message.reply_photo(Divuimg, Divu, parse_mode=ParseMode.MARKDOWN)  
 
 
   
