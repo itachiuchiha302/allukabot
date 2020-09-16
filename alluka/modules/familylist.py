ab34a288369bb539547b519676e5c196fa123bd6
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-16 13:15:53 +0530

    Update familylist.py

--- alluka/modules/familylist.py
+++ alluka/modules/familylist.py
@@ -11,8 +11,8 @@ def ud(bot: Bot, update: Update):
   message = update.effective_message
   text = message.text[len('/familylist '):]
   
-  Divuimg = "https://telegra.ph/file/4aee5cfe2ba8a3fa503d0.jpg"
-  Divu = """[Mr.Divu」](https://telegram.dog/imDivu) As *Hisoka Morow*.\n To Get More About Him Do `/info @imDivu`"""
+  Divuimg = "https://telegra.ph/file/1ffc6bb371cb4029a0c78.jpg"
+  Divu = """[Mr.Divu」](https://telegram.dog/imDivu) As *Itachi Uchiha*.\n To Get More About Him Do `/info @imDivu`"""
 
   Ayanokojiimg = "https://telegra.ph/file/1cf42dcd39ff264f5a2f1.jpg"
   Ayanokoji = """[Ayanokōji](https://telegram.dog/Ayanokoji_op) As Ayanokōji.\n To Get More About Him Do `/info @Ayanokoji_op`"""
