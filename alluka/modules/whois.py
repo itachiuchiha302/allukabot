7c03a45496daa7937ea99166b4979a86f76439f3
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-23 20:58:16 +0530

    Update whois.py

--- alluka/modules/whois.py
+++ alluka/modules/whois.py
@@ -71,17 +71,7 @@ def info(bot: Bot, update: Update, args: List[str]):
     except BadRequest:
         pass
 
-   def __user_info__(user_id):
-    text = "Currently AFK: <b>{}</b>"
-    if sql.is_afk(user_id):
-        text = text.format("Yes")
-        user = sql.check_afk_status(user_id)
-        if user.reason:
-              text += "\nReason: <code>{}</code>".format(html.escape(user.reason))
-    else:
-         text = text.format("No")
-    return text
-
+   
     if user.id == OWNER_ID:
         text += "\nThis Person is my owner - I would never do anything against them!."
         
