02970f56b5c1f148926dd02635d692ad59190590
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-23 11:08:02 +0530

    Create afk.py

---
+++ alluka/modules/afk.py
@@ -0,0 +1,178 @@
+import random
+import html
+import time
+from typing import Optional
+
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity, ParseMode
+from telegram.error import BadRequest
+from telegram.ext import Filters, MessageHandler, run_async
+
+from alluka import dispatcher
+from alluka.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from alluka.modules.sql import afk_sql as sql
+from alluka.modules.users import get_user_id
+
+
+AFK_GROUP = 7
+AFK_REPLY_GROUP = 8
+TIME = 0.0
+
+def time_formatter(seconds: float) -> str:
+    """ humanize time """
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "")
+    return tmp[:-2]
+
+@run_async
+def afk(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    args = update.effective_message.text.split(None, 1)
+    if len(args) >= 2:
+        reason = args[1]
+    else:
+        reason = ""
+
+    sql.set_afk(update.effective_user.id, reason)
+    fname = update.effective_user.first_name
+    update.effective_message.reply_text("{} is now away!".format(fname))
+
+    
+@run_async
+def no_longer_afk(bot: Bot, update: Update):
+    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat  # type: Optional[Chat]
+    message = update.effective_message  # type: Optional[Message]
+
+    if not user:  # ignore channels
+        return
+
+    res = sql.rm_afk(user.id)
+    if res:
+        if message.new_chat_members:  #dont say msg
+            return
+        firstname = update.effective_user.first_name
+        try:        
+            options = [
+          
+            '{} is here!',
+            '{} is back!',
+            '{} is now in the chat!',
+            '{} is awake!',
+            '{} is back online!',
+            '{} is finally here!',
+            'Stop! Your Discussion\n {} Is Back.',
+            'Welcome Back, {}',
+            'Wait, What??🤨 \n {} Is Back!!',
+            'Look Here Who Is Back! - {}',
+            "I Can't Believe😵 \n {} Is Back!",
+     
+                    ]
+            chosen_option = random.choice(options)
+            update.effective_message.reply_text(chosen_option.format(firstname))
+        except:
+            return
+
+
+@run_async
+def reply_afk(bot: Bot, update: Update):
+    message = update.effective_message  # type: Optional[Message]
+    userc = update.effective_user  # type: Optional[User]
+    userc_id = userc.id
+    if message.entities and message.parse_entities(
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
+        entities = message.parse_entities(
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+
+        chk_users = []
+        for ent in entities:
+            if ent.type == MessageEntity.TEXT_MENTION:
+                user_id = ent.user.id
+                fst_name = ent.user.first_name
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+                
+            if ent.type == MessageEntity.MENTION:
+                user_id = get_user_id(message.text[ent.offset:ent.offset +
+                                                   ent.length])
+                if not user_id:
+                    # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
+                    return
+                
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+
+                try:
+                    chat = bot.get_chat(user_id)
+                except BadRequest:
+                    print("Error: Could not fetch userid {} for AFK module".
+                          format(user_id))
+                    return
+                fst_name = chat.first_name
+
+            else:
+                return
+
+            check_afk(bot, update, user_id, fst_name, userc_id)
+            
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+        check_afk(bot, update, user_id, fst_name, userc_id)
+
+
+def check_afk(bot, update, user_id, fst_name, userc_id):
+    afk_time = time_formatter(round(time.time() - TIME))
+    if sql.is_afk(user_id):
+        user = sql.check_afk_status(user_id)
+        if not user.reason:
+            if int(userc_id) == int(user_id):
+                return
+            res = "{} is afk \nLast Seen: {} ago".format(fst_name, afk_time)
+            update.effective_message.reply_text(res)
+        else:
+            if int(userc_id) == int(user_id):
+                return
+            res = "{} is afk.\nLast Seen: {} ago \nReason: {}".format(fst_name, afk_time, user.reason)
+            update.effective_message.reply_text(res)
+
+def __user_info__(user_id):
+    text = "Currently AFK: <b>{}</b>"
+    if sql.is_afk(user_id):
+        text = text.format("Yes")
+        user = sql.check_afk_status(user_id)
+        if user.reason:
+              text += "\nReason: <code>{}</code>".format(html.escape(user.reason))
+    else:
+         text = text.format("No")
+    return text
+
+
+__help__ = """
+ - /afk <reason>: mark yourself as AFK(away from keyboard).
+ - brb <reason>: same as the afk command - but not a command.
+When marked as AFK, any mentions will be replied to with a message to say you're not available!
+"""
+
+AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
+AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
+
+dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
+dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
+
+__mod_name__ = "AFK"
+__command_list__ = ["afk"]
+__handlers__ = [(AFK_HANDLER, AFK_GROUP), (AFK_REGEX_HANDLER, AFK_GROUP), (NO_AFK_HANDLER, AFK_GROUP),
+                (AFK_REPLY_HANDLER, AFK_REPLY_GROUP)]
