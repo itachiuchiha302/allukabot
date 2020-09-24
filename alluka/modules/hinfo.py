fceb6d4562731270d781929df7aa492d37685db4
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-24 10:27:37 +0530

    Create hinfo.py

---
+++ alluka/modules/hinfo.py
@@ -0,0 +1,112 @@
+import html
+import json
+import os
+import psutil
+import random
+import time
+import datetime
+from typing import Optional, List
+import re
+import requests
+from telegram.error import BadRequest
+from telegram import Message, Chat, Update, Bot, MessageEntity
+import alluka.modules.helper_funcs.cas_api as cas
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown, mention_html
+from alluka.modules.helper_funcs.chat_status import user_admin, sudo_plus, is_user_admin
+from alluka import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, DEV_USERS, WHITELIST_USERS, BAN_STICKER
+from alluka.__main__ import STATS, USER_INFO, TOKEN
+from alluka.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from alluka.modules.helper_funcs.extraction import extract_user
+from alluka.modules.helper_funcs.filters import CustomFilters
+import alluka.modules.sql.users_sql as sql
+
+@run_async
+def hinfo(bot: Bot, update: Update, args: List[str]):
+    message = update.effective_message
+    chat = update.effective_chat
+    user_id = extract_user(update.effective_message, args)
+
+    if user_id:
+        user = bot.get_chat(user_id)
+
+    elif not message.reply_to_message and not args:
+        user = message.from_user
+
+    elif not message.reply_to_message and (not args or (
+            len(args) >= 1 and not args[0].startswith("@") and not args[0].isdigit() and not message.parse_entities(
+        [MessageEntity.TEXT_MENTION]))):
+        message.reply_text("I can't extract a user from this.")
+        return
+
+    else:
+        return
+
+    text = (f"<b>Characteristics :-</b>\n\n"
+            f"ID :- <code>{user.id}</code>\n"
+            f"First Name :- {html.escape(user.first_name)}")
+
+    if user.last_name:
+        text += f"\nLast Name :- {html.escape(user.last_name)}"
+
+    if user.username:
+        text += f"\nUsername  :- @{html.escape(user.username)}"
+
+    text += f"\nUser Link    :- {mention_html(user.id, ' Here 🤡')}"
+
+    
+    num_chats = sql.get_user_num_chats(user.id)
+    text += f"\n\nMutual Chats :-  <code>{num_chats}</code> \n"
+
+    try:
+        user_member = chat.get_member(user.id)
+        if user_member.status == 'administrator':
+            result = requests.post(f"https://api.telegram.org/bot{TOKEN}/getChatMember?chat_id={chat.id}&user_id={user.id}")
+            result = result.json()["result"]
+            if "custom_title" in result.keys():
+                custom_title = result['custom_title']
+                text += f"\nAdmin Title    :-  <b>{custom_title}</b> \n"
+    except BadRequest:
+        pass
+
+   
+    if user.id == OWNER_ID:
+        text += "\nThis Person is my owner - I would never do anything against them!."
+        
+    elif user.id in DEV_USERS:
+        text += "\nThis person is my dev - I would never do anything against them!."
+        
+    elif user.id in SUDO_USERS:
+        text += "\nThis person is one of my sudo users! " \
+                    "Nearly as powerful as my owner - so watch it.."
+        
+    elif user.id in SUPPORT_USERS:
+        text += "\nThis person is one of my support users! " \
+                        "Not quite a sudo user, but can still gban you off the map."
+        
+  
+       
+    elif user.id in WHITELIST_USERS:
+        text += "\nThis person has been whitelisted! " \
+                        "That means I'm not allowed to ban/kick them."
+       
+
+    
+    
+    for mod in USER_INFO:
+        if mod.__mod_name__ == "Users":
+            continue
+
+        try:
+            mod_hinfo = mod.__user_hinfo__(user.id)
+        except TypeError:
+            mod_hinfo = mod.__user_hinfo__(user.id, chat.id)
+        if mod_hinfo:
+            text += "\n" + mod_hinfo
+
+
+    update.effective_message.reply_text(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+
+HINFO_HANDLER = DisableAbleCommandHandler("hinfo", hinfo, pass_args=True)
+dispatcher.add_handler(HINFO_HANDLER)
