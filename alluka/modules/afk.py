28d274fac349cc56e3d50e051f70cb019c1889f8
Divu <70699947+imdivu@users.noreply.github.com>
2020-10-02 18:39:28 +0530

    Update afk.py

--- alluka/modules/afk.py
+++ alluka/modules/afk.py
@@ -23,9 +23,7 @@ def time_formatter(seconds: float) -> str:
     minutes, seconds = divmod(int(seconds), 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    months, days = divmod(days, 30)
-    tmp = ((str(months) + "months, ") if months else "") + \
-        ((str(days) + "d, ") if days else "") + \
+    tmp = ((str(days) + "d, ") if days else "") + \
         ((str(hours) + "h, ") if hours else "") + \
         ((str(minutes) + "m, ") if minutes else "") + \
         ((str(seconds) + "s, ") if seconds else "")
