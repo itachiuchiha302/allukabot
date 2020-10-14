36256bdb6b2ebefbc97efbabdc3a22d5ef10edd5
Divu <70699947+imdivu@users.noreply.github.com>
2020-10-14 22:17:04 +0530

    Update memes_strings.py

--- alluka/modules/helper_funcs/memes_strings.py
+++ alluka/modules/helper_funcs/memes_strings.py
@@ -169,7 +169,7 @@ SLAP_TEMPLATES = (
     "{user1} hits {user2} with {item}.",
 )
 
-GIFS = ['CgACAgUAAxkDAAEoqndfhyZfr5JyfSr8Sm2LQyoGGur2AQACVQEAAm7T2FbmKpYHSFM8PhsE']
+GIFS = ['CgACAgQAAx0CU89tcgACBgpfhys3qkFuod5wfq2mlSj94NqfuwACaAgAAlZD0VHcFt2Z4Vw1URsE']
 
 
 PING_STRING = (
