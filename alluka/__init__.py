d431522a1cfe6d566274559528c378e3826cf06d
Divu <70699947+imdivu@users.noreply.github.com>
2020-10-15 20:28:06 +0530

    Update __init__.py

--- alluka/__init__.py
+++ alluka/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-LOGGER.info("Starting alluka...")
+LOGGER.info("Starting Hinata...")
 
 # If Python version is < 3.6, stops the bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
